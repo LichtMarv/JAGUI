55cc037d50139531eb9d818d4f1b43f3da54506d
LichtMarv <mvahldiek2@gmail.com>
2022-05-24 17:11:20 +0200

    draw routine

--- src/JAGUI.lua
+++ src/JAGUI.lua
@@ -5,6 +5,21 @@ function jag:init()
     if self.screen == nil then
         self.screen = term
     end
+    self.counter = 0
+    while true do
+        jag:clear()
+        jag:draw()
+        jag:postUpdate()
+    end
+end
+
+function jag:clear()
+    self.screen.clear()
+    self.screen.setCursorPos(1, 1)
+end
+
+function jag:postUpdate()
+    self.counter = self.counter + 1
 end
 
 function jag:drawText(text)
